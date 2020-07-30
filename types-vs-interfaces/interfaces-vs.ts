454afbd9db1d3da0e63244076bdb2550303422ff
Ruiran Oliveira <ruiranoliveirasantos@gmail.com>
2020-07-30 18:52:25 -0300

    Aula09

---
+++ types-vs-interfaces/interfaces-vs.ts
@@ -0,0 +1,48 @@
+// Interfaces
+
+// definição
+interface IGame {
+    title: string;
+}
+
+interface IDLC {
+    extra: string;
+}
+
+// intersecção | extend
+interface IGameCollection extends IGame, IDLC {}
+
+// implements
+class CreateGame implements IGameCollection {}
+
+// declarar função
+interface IgetSimilars {
+    (title: string): void;
+}
+
+// Diferenças
+interface IID extends number {}
+
+// não consigo definir tuplas em interfaces
+interface ITuple {
+    0:number;
+    1:number;
+}
+
+[1, 2, 3] as ITuple;
+
+// Pode ter multiplas declaraçoes e ele une de 
+// mesmo nome
+
+interface IJQuery {
+    a: string;
+}
+
+interface IJQuery {
+    b: string;
+}
+
+const $: IJQuery = {
+    a: 'bla',
+    b: 'foo',
+}
---
+++ types-vs-interfaces/types-vs.ts
@@ -0,0 +1,33 @@
+// Type Alias
+
+// definição
+type TGame = {
+    title: string;
+};
+
+type TDLC = {
+    title: string;
+};
+
+// intersection
+type TGameCollection = TGame & TDLC;
+
+// implements
+class TCreateGame implements TGameCollection {}
+
+// declarar função
+type getSimilars = (title: string) => void;
+
+// Diferenças
+
+// permite declarar tipos primitivos
+type TID = string | number;
+
+// pode declarar tuplas normalmente
+type TTuple = [number, number];
+
+[1, 2] as TTuple;
+
+// Apenas uma declaração por escopo
+type TJQuery = { a: string };
+type TJQuery = { b: string };
